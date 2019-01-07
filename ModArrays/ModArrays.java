e81918ced1040de812a3597694ee4dd8b1d16c4d
Omar21 <bubbathebeast02143@gmail.com>
2019-01-07 13:00:53 -0800

    Mod arrays

---
+++ ModArrays/ModArrays.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class ModArrays here.
+ *
+ * @author (Omar Lara)
+ * @version (1/7/19)
+ */
+public class ModArrays
+{
+int[] arr = {4,7,8,9,3,0};
+
+    public void add(int num, int index){
+        
+    }
+    
+    public void remove(int index){
+        
+    }
+}
