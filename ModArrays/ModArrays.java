2f85f0ff95e36d1aaff9c91770de5c5056031eaf
OmarLara21 <bubbathebeast02143@gmail.com>
2019-01-09 01:38:34 -0800

    messing around

--- ModArrays/ModArrays.java
+++ ModArrays/ModArrays.java
@@ -7,13 +7,15 @@
  */
 public class ModArrays
 {
-int[] arr = {4,7,8,9,3,0};
+    int[] arr = {4,7,8,9,3,0};
 
     public void add(int num, int index){
         
     }
     
     public void remove(int index){
-        
+        for(int i = 0; int i <arr.length; i+=1){
+            System.out.println(arr[i]);
+        }
     }
 }
---
+++ ModArrays/modarraysV2.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class ModArrays here.
+ *
+ * @author (Omar Lara)
+ * @version (1/7/19)
+ */
+public class modarraysV2
+{
+    int[] array = {4,7,8,9,3,0};
+    public void remove(int index){
+        for(int i = 0;i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
