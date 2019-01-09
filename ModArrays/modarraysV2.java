7dc7c5b696276c9ca41c07d0d78860d027297e64
Omar21 <bubbathebeast02143@gmail.com>
2019-01-09 13:24:26 -0800

    this works

--- ModArrays/modarraysV2.java
+++ ModArrays/modarraysV2.java
@@ -9,9 +9,10 @@ public class modarraysV2
 {
     int[] array = {4,7,8,9,3,0};
     public void add(int index, int num){
-        for(int i = index; i <array.length-1;i++){
-            array[i] = num;
+        for(int i = array.length-1; i >index;i--){
+            array[i] = array[i-1];
         }
+        array[index] = num;
     }
     public void remove(int index){
         for(int i = index;i < array.length-1; i++){
